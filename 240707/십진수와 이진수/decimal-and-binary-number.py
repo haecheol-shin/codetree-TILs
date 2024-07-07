2a94e629df2e2faa3d9915c9343060ff53788327
Haecheol Shin <49307997+haecheol-shin@users.noreply.github.com>
2024-07-07 14:52:01 +0900

    Title: 십진수와 이진수, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240707/십진수와 이진수/decimal-and-binary-number.py
@@ -0,0 +1,21 @@
+import sys
+
+binary = sys.stdin.readline().rstrip("\n")
+
+decimal = 0
+for i in range(len(binary)):
+    decimal += int(binary[i]) * 2**(len(binary)-i-1)
+
+decimal *= 17
+
+result = []
+while decimal / 2 != 0:
+    if decimal % 2 == 0:
+        result.append(0)
+    else:
+        result.append(1)
+
+    decimal = int(decimal / 2)
+
+for i in range(len(result)):
+    print(result.pop(), end="")
