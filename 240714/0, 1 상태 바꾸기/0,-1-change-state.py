c6be122bf3030c5b11bc45d262fbc35f9e17066a
Haecheol Shin <49307997+haecheol-shin@users.noreply.github.com>
2024-07-14 22:10:17 +0900

    Title: 0, 1 상태 바꾸기, Time: 130ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240714/0, 1 상태 바꾸기/0,-1-change-state.py
@@ -0,0 +1,27 @@
+import sys
+
+n_m = list(map(int, sys.stdin.readline().split()))
+
+n = n_m[0]
+m = n_m[1]
+
+result = list(map(int, sys.stdin.readline().split()))
+
+for i in range(m):
+    a_b_c = list(map(int, sys.stdin.readline().split()))
+    a = a_b_c[0]
+    b = a_b_c[1]
+    c = a_b_c[2]
+
+    if a == 1:
+        result[b-1] = c
+    
+    else:
+        for i in range(b-1,c):
+            if result[i] == 0:
+                result[i] = 1
+            else:
+                result[i] = 0
+
+for i in result:
+    print(i, end = " ")
