33a42c47ebe8430cdfcf06c160f7f583b3f02aba
Haecheol Shin <49307997+haecheol-shin@users.noreply.github.com>
2024-07-28 01:11:28 +0900

    Title: 괄호 문자열의 적합성 판단, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240728/괄호 문자열의 적합성 판단/parentheses-string.py
+++ 240728/괄호 문자열의 적합성 판단/parentheses-string.py
@@ -1,19 +1,21 @@
 import sys
 
-sentence = sys.stdin.readline()
-
-stack = []
-
-for i in sentence:
-    if i == '(':
-        stack.append(i)
-    else:
-        if len(stack) == 0 or stack[-1] != '(':
-            print("No")
+def func(sentence):
+    stack = []
+    for i in sentence:
+        if i == '(':
+            stack.append(i)
         else:
-            stack.pop()
-        
-if len(stack) != 0:
-    print("No")
-else:
-    print("Yes")
+            if len(stack) == 0 or stack[-1] != '(':
+                return print("No")
+            else:
+                stack.pop()
+            
+    if len(stack) != 0:
+        return print("No")
+    else:
+        return print("Yes")
+
+def __main__:
+    sentence = sys.stdin.readline()
+    func(sentence)
