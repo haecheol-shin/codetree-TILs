53f5a6bd273d897d780b085746ada02446e38889
Haecheol Shin <49307997+haecheol-shin@users.noreply.github.com>
2024-09-02 23:59:50 +0900

    Title: 작은 구슬의 이동, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240902/작은 구슬의 이동/small-marble-movement.py
+++ 240902/작은 구슬의 이동/small-marble-movement.py
@@ -5,21 +5,21 @@ d_dict = {'U': 1, 'D': 2, 'R': 0, 'L': 3}
 dx = [1, 0, 0, -1]
 dy = [0, 1, -1, 0]
 
-x = int(c) - 1
-y = int(r) - 1
+x = int(r) - 1
+y = int(c) - 1
 
 def in_range(x, y):
     return x>=0 and x<n and y>=0 and y<n
 
 for i in range(t):
-    nx = x + dx[d_dict[d]]
-    ny = y + dy[d_dict[d]]
-
-    if in_range(nx, ny) is False:
+    if in_range(x, y) is False:
         d_dict[d] = 3 - d_dict[d]
-
+    
     else:
+        nx = x + dx[d_dict[d]]
+        ny = y + dy[d_dict[d]]
+
         x = nx
         y = ny
 
-print(y+1, x+1)
+print(x+2, y+2)
